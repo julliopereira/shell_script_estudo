b284483bc24b86a3fd593bd9ef81090506fbe5d9
Julio Cesar Pereira <julliopereira@gmail.com>
2022-09-17 17:30:01 -0300

    Atualização para GitHub

--- COMANDOS_UTEIS/00001_ping_audivel.sh
+++ COMANDOS_UTEIS/00001_ping_audivel.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
-
+#
 # AUTHOR:                   JULIO C. PEREIRA
 # INICIO:                   2022-09-17
 # CONTATO:                  julliopereira@gmail.com
 # OBJETIVO:                 MONITORAR CONEXOES POR ICMP
 #                           COM NAO SUCESSO AUDIVEL GRAVANDO
 #                           RESULTADO EM ARQUIVOS down E up
-################################################################
+#################################################################
 #
-#INPUT ========================================================
+# CONTROLE DE DIRETORIO: ========================================
+if [ ! -e log/ ]: then
+    mkdir log
+fi
+#
+# TRATAMENTO DE INPUT: ==========================================
 IP=$1
 NR=$2
 MTU=$3
+#
+# DEFINICOES DE VARIAVEIS:
 FUSO=0
-#FUNCOES ======================================================
+#
+# FUNCOES: ======================================================
 func_data() {
     DATA=$(date -d "$FUSO hour" +%Y%m%d_%H:%M:%S)
 }
-#
+# MAIN: =========================================================
 for ((i=0;i<=$2;i++)); do
-    ping $IP -c 1 -i 0.25 -W 0.1 &> /dev/null
+    ping $IP -c 1 -i 0.5 -W 0.6 &> /dev/null
     if [ $? -eq 0 ]; then
         func_data
-        echo -e 
-        echo -e "$i:\t[$DATA] \t: UP" > /dev/null
+        echo -e "$i:\t[$DATA] \t: UP" > log/log$DATA.log
     else
         func_data
-        echo -e "$i:\t[$DATA] \t: DW \a" > /dev/null
+        echo -e "$i:\t[$DATA] \t: DW \a" > log/log$DATA.log
     fi
 done
 
