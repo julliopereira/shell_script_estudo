99413a7fb6fae0d97c734b86264b482e87bda101
Julio Cesar Pereira <julliopereira@gmail.com>
2022-09-17 19:30:01 -0300

    Atualização para GitHub

--- COMANDOS_UTEIS/00001_ping_audivel.sh
+++ COMANDOS_UTEIS/00001_ping_audivel.sh
@@ -28,25 +28,26 @@ func_data() {
 }
 #
 func_filtra() {
-    for linha in $(cat NEs | grep -v "#" > /tmp/nes); do 
-        IP="IP"
-        MTU="MTU MINIMO"
-        TM="TEMPO MEDIO DE RTT"
-        TR="REMPO A REDUZIR"
-        NOME="NOME OU INFORMACOES DO DESTINO"
-    done
+    IP="cat /tmp/nes | cut -d ":" -f 1"
+    MTU="cat /tmp/nes | cut -d ":" -f 2"
+    TM="cat /tmp/nes | cut -d ":" -f 3"
+    TR="cat /tmp/nes | cut -d ":" -f 4"
+    NOME="cat /tmp/nes | cut -d ":" -f 5"
 }
 # MAIN: =========================================================
-# while true:               # COMENTAR A LINHA for LOGO ABAIXO PARA LOOP INFINITO
-for ((i=0;i<=$2;i++)); do   
-    ping $IP -c 2 -i 0.2 -W 0.5 &> /dev/null
-    if [ $? -eq 0 ]; then
-        func_data
-        echo -e "[$DATAS]:$IP:UP" >> log/log$DATA.log
-    else
-        func_data
-        echo -e "[$DATAS]:$IP:DW \a" >> log/log$DATA.log
-    fi
+while true; do               # COMENTAR A LINHA for LOGO ABAIXO PARA LOOP INFINITO
+#for ((i=0;i<=$2;i++)); do
+    for linha in $(cat NEs | grep -v "#" > /tmp/nes); do
+        func_filtra
+        ping $IP -c 2 -i 0.2 -W 0.5 &> /dev/null
+        if [ $? -eq 0 ]; then
+            func_data
+            echo -e "[$DATAS]:$IP:UP" >> log/log$DATA.log
+        else
+            func_data
+            echo -e "[$DATAS]:$IP:DW \a" >> log/log$DATA.log
+        fi
+    done
 done
 
 
