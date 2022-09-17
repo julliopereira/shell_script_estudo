b4105d219100c798f7d2242fc908e29480cb490a
Julio Cesar Pereira <julliopereira@gmail.com>
2022-09-17 16:00:01 -0300

    Atualização para GitHub

---
+++ COMANDOS_UTEIS/00001_ping_audivel.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# AUTHOR:                   JULIO C. PEREIRA
+# INICIO:                   2022-09-17
+# CONTATO:                  julliopereira@gmail.com
+# OBJETIVO:                 MONITORAR CONEXOES POR ICMP
+#                           COM NAO SUCESSO AUDIVEL GRAVANDO
+#                           RESULTADO EM ARQUIVOS down E up
+################################################################
+
+for 
+
+
+
+
+
+# COMMANDO:
+# for ((i=0;i<1000;i++)); do ping 8.8.8.8 -c 1 -i 0.015 -W 0.005 &> /dev/null || echo -e "\a $i $?" ; done
