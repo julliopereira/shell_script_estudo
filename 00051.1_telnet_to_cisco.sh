167b314a5bd428f705ca64665a3f647291590b17
Julio Cesar Pereira <julliopereira@gmail.com>
2022-09-21 23:30:01 -0300

    Atualização para GitHub

---
+++ 00051.1_telnet_to_cisco.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#
+#
+#
+# INPUTS:
+read -p "Digite seu login: " LOGIN 
+read -s -p "Digite sua senha: " SENHA
+#
+#
+#
+# FUNCOES:
+func_commandos() {
+    echo
+}
+echo -e "$LOGIN $SENHA"
