f67e4804ebda9a592407af5910f395b5365a51e3
Julio Cesar Pereira <julliopereira@gmail.com>
2022-10-02 23:30:01 -0300

    Atualização para GitHub

--- 00051.1_telnet_to_cisco.sh
+++ 00051.1_telnet_to_cisco.sh
@@ -8,16 +8,18 @@ read -s -p "password: " SENHA
 #
 # FUNCOES:
 func_commandos() {
+    # EXECUTA O COMANDO REDIGIDO EM CADA linha DO ARQUIVO telnet_cmds.txt
     for comm in $(cat telnet_cmds.txt | tr ' ' '_'); do
         command=$(echo -e "$comm" | tr '_' ' ')
         echo -e "$command"; sleep 1
     done
 }
 #
-(   sleep 1
+( 
     echo -e "$LOGIN"
     echo -e "$SENHA"; sleep 1
+    
     func_commandos
-) | telnet 10.0.3.2 > show.txt
-cat show.txt
+) | telnet 10.0.3.2 > show.txt               # SALVA RESULTADO NO ARQUIVO show.txt
+cat show.txt                                 # MOSTRA CONTEUDO DO ARQUIVO show.txt
 
