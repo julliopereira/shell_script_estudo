34896f26e4e99073b806cf3d4e304a7dbf4701ea
Julio Cesar Pereira <julliopereira@gmail.com>
2022-10-24 00:30:01 -0300

    Atualização para GitHub

--- 00008.0_function.sh
+++ 00008.0_function.sh
@@ -3,13 +3,18 @@
  #PODEMOS CRIAR FUCNÇÕES DE DUAS FORMAS
 
  function funcao_x() {
-
+    echo "funcao 1"
  }
 
  #OU
 
  funcao_y () {
-    
+    echo "funcao 2"
  }
+
+ # E CHAMAMOS A FUNÇÃO DA SEGUNTE FORMA
+
+ funcao_x               # REALIZARÁ A FUNCAO X
+ funcao_y               # REALIZARÁ A FUNCAO Y
 
  
