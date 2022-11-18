525c809362fa93226b107829b51dcde250ab4e89
Julio Cesar Pereira <julliopereira@gmail.com>
2022-11-18 15:37:05 -0300

    captura de teclas

--- 00060.1_captura_de_teclas_menu.sh
+++ 00060.1_captura_de_teclas_menu.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-#!/bin/bash
 #
 echo ":: Qual o a opcao desejada?: "
 tecla=""
@@ -10,13 +8,13 @@ stty -echo -icanon min 0
 
 while true
 do	
-	clear
 	[ "$tecla" = "s" ] && break
 	[ "$tecla" = "1" ] && break
 	[ "$tecla" = "2" ] && break
 	[ "$tecla" = "3" ] && break
 	[ "$tecla" = "4" ] && break	
 	[ "$tecla" = "5" ] && break
+	clear
 	echo -e "[01] opcao 1"
 	echo -e "[02] opcao 2"
 	echo -e "[03] opcao 3"
@@ -24,7 +22,7 @@ do
 	echo -e "[05] opcao 5\n"
 	echo -e "[ s ]  sair \n"
 	echo -n "opc: "
-	sleep 1
+	sleep 0.5
 	read tecla 
 	T1=$tecla
 done
@@ -32,7 +30,10 @@ done
 # Restaura o modo padrao
 stty sane
 
+echo -e "\n\n"
+
 echo -e "você digitou \"$T1\""
-echo "Tchau loop."
+
+echo -e "\nTchau loop."
 
 exit 0
